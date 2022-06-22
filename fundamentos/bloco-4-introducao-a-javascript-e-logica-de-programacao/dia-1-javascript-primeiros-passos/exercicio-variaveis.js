339b7ceca39593cb5562fc52ca8e41d93a02a9cc
Pétala Leite <petalaleite@gmail.com>
2022-06-22 13:32:23 -0300

    Exercício vamos praticar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-variaveis.js
@@ -0,0 +1,11 @@
+const myName = 'Pétala';
+const birthCity = 'Manga';
+const birthYear = 1991;
+
+console.log(myName, birthCity, birthYear)
+
+birthYear = 2030;
+console.log(birthYear)
+
+birthCity = 'Manchester';
+console.log(birthCity)
