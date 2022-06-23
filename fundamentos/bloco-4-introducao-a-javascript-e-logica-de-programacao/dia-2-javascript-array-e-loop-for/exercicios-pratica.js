b87a24f6dbb4822a1a1475fd6b866e85fc09c00f
Pétala Leite <petalaleite@gmail.com>
2022-06-23 11:50:14 -0300

    Questao 2 e 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-pratica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-pratica.js
@@ -1,5 +1,25 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// Questão 1
+
 for (let i = 0; i < numbers.length; i += 1) {
 	console.log(numbers[i]);
 }
+
+// Questão 2
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+	soma = soma + numbers[i];
+}
+console.log(soma);
+
+// Questão 3
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+	soma = soma + numbers[i];
+}
+console.log(soma / numbers.length);
