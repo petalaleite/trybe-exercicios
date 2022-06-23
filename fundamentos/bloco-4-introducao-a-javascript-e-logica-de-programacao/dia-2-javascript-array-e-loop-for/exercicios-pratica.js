e46bc341f18fb754d26b5be3a3c544fc6bb082b7
Pétala Leite <petalaleite@gmail.com>
2022-06-23 12:16:54 -0300

    Questao 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-pratica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-pratica.js
@@ -23,3 +23,18 @@ for (let i = 0; i < numbers.length; i += 1) {
 	soma = soma + numbers[i];
 }
 console.log(soma / numbers.length);
+
+// Questão 4
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+	soma += numbers[i];
+}
+let media = soma / numbers.length;
+
+if (media > 20) {
+	console.log("Valor maior que 20");
+} else {
+	console.log("Valor menor ou igual a 20");
+}
