8a4082910a1b232837fddfa3625504d73a210785
Pétala Leite <petalaleite@gmail.com>
2022-06-27 13:08:30 -0300

    Questão 6 resolvida

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-ii-funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-ii-funcoes.js
@@ -10,13 +10,13 @@
 array = [2, 3, 6, 7, 10, 1];
 
 function retornaMaiorValor(array) {
-	let maiorValor = array[0];
+	let indiceMaiorValor = 0;
 	for (let i = 0; i < array.length; i += 1) {
-		if (array[i] > maiorValor) {
-			maiorValor = array[i];
+		if (array[indiceMaiorValor] < array[i]) {
+			indiceMaiorValor = i;
 		}
 	}
-	return maiorValor;
+	return indiceMaiorValor;
 }
 
 console.log(retornaMaiorValor(array));
@@ -26,17 +26,53 @@ console.log(retornaMaiorValor(array));
 arrayTeste = [2, 4, 6, 7, 10, 0, -3];
 
 function retornaMenorValor(array) {
-	let menorValor = array[0];
+	let indiceMenorValor = array[0];
 	for (let i = 0; i < array.length; i += 1) {
-		if (array[i] < menorValor) {
-			menorValor = array[i];
+		if (array[indiceMenorValor] > array[i]) {
+			indiceMenorValor = i;
 		}
 	}
-	return menorValor;
+	return indiceMenorValor;
 }
 
 console.log(retornaMenorValor(arrayTeste));
 // Questão 4
+
+let arrayNomes = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"];
+
+function retorneMaiorCaracteres(array) {
+	let maiorLength = array[0];
+	for (let i in array) {
+		if (maiorLength.length < array[i].length) {
+			maiorLength = array[i];
+		}
+	}
+	return maiorLength;
+}
+
+console.log(retorneMaiorCaracteres(arrayNomes));
+
 // Questão 5
+
+// arrayQuestao5 = [2, 3, 2, 5, 8, 2, 3];
+
+// function retornaMaisRepetido(array) {
+//   for (let i in array) {
+//     if ()
+//   }
+// }
 // Questão 6
+
+function somaNatural(n) {
+	let soma = 0;
+	for (let i = 1; i <= n; i += 1) {
+		soma = soma + i;
+	}
+
+	return soma;
+}
+
+console.log(somaNatural(5));
 // Questão 7
+
+function verificaString(word, ending) {}
