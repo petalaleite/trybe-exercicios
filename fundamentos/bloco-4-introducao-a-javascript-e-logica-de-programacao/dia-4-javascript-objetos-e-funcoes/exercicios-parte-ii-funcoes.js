531462a04151630d39631710e3f5c66a81a5b102
Pétala Leite <petalaleite@gmail.com>
2022-06-27 10:48:45 -0300

    Questão 2 resolvida

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-ii-funcoes.js
@@ -0,0 +1,27 @@
+// Questão 1
+
+// function verificaPalindromo(palavra) {
+//   if() {
+//     return true;
+//   }
+// }
+// Questão 2
+
+array = [2, 3, 6, 7, 10, 1];
+
+function retornaMaiorValor(array) {
+	let maiorValor = array[0];
+	for (let i = 0; i < array.length; i += 1) {
+		if (array[i] > maiorValor) {
+			maiorValor = array[i];
+		}
+	}
+	return maiorValor;
+}
+
+console.log(retornaMaiorValor(array));
+// Questão 3
+// Questão 4
+// Questão 5
+// Questão 6
+// Questão 7
