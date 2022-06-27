0da810af00ad3c70223b68e62e6777359ac945e0
Pétala Leite <petalaleite@gmail.com>
2022-06-27 10:24:04 -0300

    Questão 6 resolvida

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-i-objetos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-i-objetos.js
@@ -26,3 +26,36 @@ for (let key in info) {
 }
 
 // Questão 5
+
+// let info2 = {
+// 	personagem: "Tio Patinhas",
+// 	origem: "Christmas on Bear Mountain,  Dell's Four Color Comics #178",
+// 	nota: "O último MacPatinhas",
+// 	recorrente: "Sim",
+// };
+
+// for (key in );
+
+// Questão 6
+
+let leitor = {
+	nome: "Julia",
+	sobrenome: "Pessoa",
+	idade: 21,
+	livrosFavoritos: [
+		{
+			titulo: "O Pior Dia de Todos",
+			autor: "Daniela Kopsch",
+			aditora: "Tordesilhas",
+		},
+	],
+};
+
+console.log(
+	"O livro favorito de " +
+		leitor.nome +
+		" " +
+		leitor.sobrenome +
+		" se chama " +
+		leitor.livrosFavoritos[0]["titulo"]
+);
