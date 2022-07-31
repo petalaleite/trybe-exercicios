bd8e01bbe4d7bf22ead74cef2375ed55ae80d2a1
Pétala Leite <petalaleite@gmail.com>
2022-07-31 12:17:47 -0300

    Questão 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios8.2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios8.2.js
@@ -83,3 +83,22 @@ function smallerName() {
 }
 
 console.log(smallerName());
+
+// questão 3
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((books) => books.name.length === 26);
+}
+
+console.log(getNamedBook());
