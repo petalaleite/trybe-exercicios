528b700d356ec7f7fb4b561ccc9257f33f5cf16b
Pétala Leite <petalaleite@gmail.com>
2022-08-02 23:34:42 -0300

    Questão 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
@@ -85,3 +85,12 @@ const myList = [1, 2, 3];
 
 const swap = ([first, second, third]) => [third, second, first];
 console.log(swap(myList));
+
+// questão 6
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ((...[ name, brand, year ]) => ({ name, brand, year }));
+console.log(toObject(palio, shelbyCobra, chiron));
