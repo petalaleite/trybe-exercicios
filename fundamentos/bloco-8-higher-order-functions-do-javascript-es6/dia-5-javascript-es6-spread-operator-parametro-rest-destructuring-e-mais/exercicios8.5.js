34b299d90ec662d3aed9edc0d470a890666a8c66
Pétala Leite <petalaleite@gmail.com>
2022-08-02 23:04:20 -0300

    Questão 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
@@ -8,7 +8,7 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  rectangleArea(...rectangles) // altere a chamada da funcao rectangleArea
+  rectangleArea(...rectangles) 
   console.log(rectangle[0] * rectangle[1]);
 }); rectangles
 
@@ -17,3 +17,25 @@ rectangles.forEach((rectangle) => {
 const sum = (...num) => num.reduce((a,b) => a + b);
 
 console.log(sum(1,2,34,5,6,6,7,8));
+
+// questão 3
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+
+const personLikes = ({ name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex)); 
+console.log(personLikes(gunnar)); 
