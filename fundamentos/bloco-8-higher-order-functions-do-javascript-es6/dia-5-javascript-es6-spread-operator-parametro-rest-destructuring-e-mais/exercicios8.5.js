21619486a4f182de73faf835390c9bf332bc2693
Pétala Leite <petalaleite@gmail.com>
2022-08-02 23:18:29 -0300

    Questão 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios8.5.js
@@ -39,3 +39,42 @@ const personLikes = ({ name, age, likes}) => `${name} is ${age} years old and li
 
 console.log(personLikes(alex)); 
 console.log(personLikes(gunnar)); 
+
+// questão 4
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (people) => people.filter(
+  ({ bornIn, nationality }) =>
+    bornIn > 1900 
+    && bornIn <= 2000 
+    && nationality === 'Australian',
+);
+
+console.log(filterPeople(people));
